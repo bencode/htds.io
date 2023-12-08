22ccc00c894c38ef70a871c8d076e0aed7328d19
bencode <bencode@163.com>
2023-12-08 14:35:01 +0800

    chore: init project

--- src/htds/ui/app.cljs
+++ src/htds/ui/app.cljs
@@ -3,12 +3,25 @@
             ["react-dom/client" :as rdom]
             [reagent.core :as r]))
 
-(defn my-component []
-  [:div "Hello World!"])
+(defn header []
+  [:h1 {:style {:color "blue"}}
+   [:div "Hello World!"]])
+
+(defn counter [n]
+  (let [count (r/atom n)]
+    (fn []
+      [:<>
+       [:div @count]
+       [:button {:on-click #(swap! count inc)} "Click Me"]])))
+
+(defn app []
+  [:<>
+   [header]
+   [counter 200]])
 
 (defn ^:dev/after-load render []
   (let [el (.getElementById js/document "root")
         root (.createRoot rdom el)]
-    (.render root (r/as-element [my-component]))))
+    (.render root (r/as-element [app]))))
 
 (defn ^:export init [] (render))
