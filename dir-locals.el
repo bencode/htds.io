9d6749368eec75bfb2f3b0e25553f840e4e33e45
bencode <bencode@163.com>
2023-12-08 13:36:16 +0800

    init first component

---
+++ dir-locals.el
@@ -0,0 +1,1 @@
+(setq cider-clojurec-eval-destination 'cljs)
--- src/htds/ui/app.cljs
+++ src/htds/ui/app.cljs
@@ -1,4 +1,14 @@
-(ns htds.ui.app)
+(ns htds.ui.app
+  (:require [react :as react]
+            ["react-dom/client" :as rdom]
+            [reagent.core :as r]))
 
-(defn init []
-  (println "Hello World"))
+(defn my-component []
+  [:div "Hello World!"])
+
+(defn ^:dev/after-load render []
+  (let [el (.getElementById js/document "root")
+        root (.createRoot rdom el)]
+    (.render root (r/as-element [my-component]))))
+
+(defn ^:export init [] (render))
